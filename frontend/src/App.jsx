5f7ab07df95dc497f49d4a520c5ea823ad413eaf
Isaiah M. Bernal <isaiahmbernal@gmail.com>
2025-11-02 11:33:29 -0500

    :tada: Yao Learning React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,40 @@
-import { useRef, useState } from "react";
+import { useReducer, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+
+  const [count, setCount] = useState(5);
+
+  const [iss, goobleGoo] = useState({
+    message: "idle",
+    latitude: 100,
+    longitude: 200,
+  });
+
+  const issData = async () => {
+
+    console.log("Logging into the server");
+
+    const response = await fetch('http://api.open-notify.org/iss-now.json');
+    const data = await response.json();
+
+    // const newData = {
+    //   ...iss,
+    //   latitude: data.iss_position.latitude,
+    //   longitude: data.iss_position.longitude
+    // }
+
+    // const newData = {
+    //   message: data.message,
+    //   latitude: data.iss_position.latitude,
+    //   longitude: data.iss_position.longitude
+    // };
+
+    goobleGoo(newData);
+
+    console.log(data);
+
+  };
 
   return (
     <>
@@ -11,10 +43,17 @@ function App() {
         <span className="emoji">🐵</span>
       </div>
       <h1>Yao + Isaiah</h1>
-      <div className="card">
+      <div>
+        <h2>ISS Location</h2>
+        <p>Status: {iss.message}</p>
+        <p>Latitude: {iss.latitude}</p>
+        <p>Longitude: {iss.longitude}</p>
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", gap: "1rem" }}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={issData}>Login</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>

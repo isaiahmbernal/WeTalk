97da9d86f168395686ab2aa2a9fa5b578fe4105c
Isaiah M. Bernal <isaiahmbernal@gmail.com>
2025-11-02 12:03:54 -0500

    :tada: Yao Learning React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { useReducer, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-
   const [count, setCount] = useState(5);
 
   const [iss, goobleGoo] = useState({
@@ -11,11 +10,30 @@ function App() {
     longitude: 200,
   });
 
-  const issData = async () => {
+  const [messages, setMessages] = useState([
+    {
+      type: "received",
+      user: "Yao",
+      content: "hey cutie",
+    },
+    {
+      type: "received",
+      user: "Yao",
+      content: "what's up",
+    },
+    {
+      type: "sent",
+      user: "Isaiah",
+      content: "nuthin much",
+    },
+  ]);
 
+  console.log(messages);
+
+  const issData = async () => {
     console.log("Logging into the server");
 
-    const response = await fetch('http://api.open-notify.org/iss-now.json');
+    const response = await fetch("http://api.open-notify.org/iss-now.json");
     const data = await response.json();
 
     // const newData = {
@@ -33,7 +51,6 @@ function App() {
     goobleGoo(newData);
 
     console.log(data);
-
   };
 
   return (
@@ -43,6 +60,36 @@ function App() {
         <span className="emoji">🐵</span>
       </div>
       <h1>Yao + Isaiah</h1>
+      <div
+        style={{
+          background: "",
+          display: "flex",
+          flexDirection: "column",
+          gap: "1rem"
+        }}
+      >
+        asdf
+        {messages.map((message) => {
+          if (message.type === "received") {
+            return (
+              <div className="">
+                <div className="message message-rec">
+                  <p>{message.content}</p>
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div className="message-row-sent">
+                <div className="message message-sent">
+                  <p>{message.content}</p>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+
       <div>
         <h2>ISS Location</h2>
         <p>Status: {iss.message}</p>
